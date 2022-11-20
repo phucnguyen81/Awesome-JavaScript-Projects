e68262dc15401961ba8373c16e587f50615085d4
Phuc K Nguyen <phucknguyen@gmail.com>
2022-11-20 14:20:21 +0700

    Go over AgeCalculator

--- assets/js/AgeCalculator.js
+++ assets/js/AgeCalculator.js
@@ -10,27 +10,27 @@ function cal() {
         alert("Please Enter a Valid Date");
     } else {
         document.getElementById('bottom').style.display = "block";
-        var a = new Date(bd.value);
-        var b = new Date(dob.value);
+        var a = new Date(bd.value);  // bd = the form input with id=bd
+        var b = new Date(dob.value);  // dob = the form input with id=dob
         var r = a.getTime() - b.getTime();
         var u = new Date(r);
         var v = new Date(0);
         var year = u.getFullYear() - v.getFullYear();
         var month = u.getMonth() - v.getMonth();
         var day = u.getDate() - v.getDate();
-        document.getElementById('age').innerHTML = year + " Years ," + month + " Months ," + day + " Days .";
+        document.getElementById('age').innerHTML = year + " Years ," + month + " Months ," + day + " Days";
         var m = (year * 12) + month
-        document.getElementById('months').innerHTML = m + " Months, " + day + " Days .";
+        document.getElementById('months').innerHTML = m + " Months, " + day + " Days";
         var w = (m * 4.345);
         w = Math.round(w);
-        document.getElementById('weeks').innerHTML = w + " Weeks, " + day + " Days ."
+        document.getElementById('weeks').innerHTML = w + " Weeks, " + day + " Days"
         var d = (w * 7) + day;
-        document.getElementById('days').innerHTML = d + " Days."
+        document.getElementById('days').innerHTML = d + " Days"
         var h = d * 24;
-        document.getElementById('hours').innerHTML = h + " Hours."
+        document.getElementById('hours').innerHTML = h + " Hours"
         var mi = h * 60;
-        document.getElementById('minutes').innerHTML = mi + " Minutes."
+        document.getElementById('minutes').innerHTML = mi + " Minutes"
         var s = mi * 60;
-        document.getElementById('seconds').innerHTML = s + " Seconds."
+        document.getElementById('seconds').innerHTML = s + " Seconds"
     }
 }
