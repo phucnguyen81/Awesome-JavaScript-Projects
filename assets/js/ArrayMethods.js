189ffd69761088b306d257b2764b9c29179d1a84
Phuc K Nguyen <phucknguyen@gmail.com>
2022-11-21 08:22:04 +0700

    Refactor a bit

--- assets/js/ArrayMethods.js
+++ assets/js/ArrayMethods.js
@@ -1,87 +1,111 @@
-const main = document.getElementById("main");
-const addUserBtn = document.getElementById("add-user");
-const doubleBtn = document.getElementById("double");
-const showMillionairesBtn = document.getElementById("show-millionaires");
-const sortBtn = document.getElementById("sort");
-const calculateWealthBtn = document.getElementById("calculate-wealth");
+/**
+ * Demontrate some common array opertions.
+ */
+class ArrayMethods {
+  constructor(doc) {
+    this.doc = doc;
+    this.data = [];
+    this.main = this.doc.getElementById("main");
 
-let data = [];
+    const addUserBtn = this.doc.getElementById("add-user");
+    const doubleBtn = this.doc.getElementById("double");
+    const showMillionairesBtn = this.doc.getElementById("show-millionaires");
+    const sortBtn = this.doc.getElementById("sort");
+    const calculateWealthBtn = this.doc.getElementById("calculate-wealth");
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
+    addUserBtn.addEventListener("click", () => this.addRandomUser());
+    doubleBtn.addEventListener("click", () => this.doubleMoney());
+    sortBtn.addEventListener("click", () => this.sortByRichest());
+    showMillionairesBtn.addEventListener("click", () => this.showMillionaires());
+    calculateWealthBtn.addEventListener("click", () => this.calculateWealth());
+    
+    // Start with some random users
+    [1,2,3].forEach(() => this.addRandomUser());
+  }
 
-async function getRandomUser() {
-  const res = await fetch("https://randomuser.me/api");
-  const data = await res.json();
-  console.log(data);
+  // Fetch a random user from public API
+  async fetchRandomUser(callback) {
+    const res = await fetch("https://randomuser.me/api");
+    const data = await res.json();
+    const user = data.results[0];
+    const newUser = {
+      name: `${user.name.first} ${user.name.last}`,
+      money: Math.floor(Math.random() * 1000000),
+    };
+    callback(newUser);
+  }
 
-  const user = data.results[0];
-  console.log(user);
+  addUser(user) {
+    this.data.push(user);
+    this.updateDOM();
+  }
 
-  const newUser = {
-    name: `${user.name.first} ${user.name.last}`,
-    money: Math.floor(Math.random() * 1000000),
-  };
+  // Add a random user
+  addRandomUser() {
+    this.fetchRandomUser(user => this.addUser(user));
+  }
 
-  addData(newUser);
+  doubleMoney() {
+    this.data = this.data.map((user) => {
+      return { ...user, money: user.money * 2 };
+    });
+    this.updateDOM();
+  }
+
+  sortByRichest() {
+    this.data.sort((a, b) => b.money - a.money);
+    this.updateDOM();
+  }
+  
+  showMillionaires() {
+    this.data = this.data.filter((user) => user.money > 1000000);
+    this.updateDOM();
+  }
+
+  // Show total money of all users
+  calculateWealth() {
+    // remove the previously added element if any
+    this.doc.getElementById("wealth")?.remove();
+
+    const wealth = this.data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthE1 = this.doc.createElement("div");
+    wealthE1.innerHTML = `<h3 id="wealth"> Total Wealth: <strong>${this.formatMoney(
+      wealth
+    )}</strong></h3>`;
+    this.main.appendChild(wealthE1);
+  }
+
+  /**
+   * Render the array of users.
+   * 
+   * This function is called to update the UI when there
+   * are changes to the user list.
+   * 
+   * @param {Array} provideData : the array of users
+   */
+  updateDOM(provideData) {
+    provideData = provideData || this.data;
+
+    // clear previous data
+    this.main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+    // add new data
+    provideData.forEach((item) => {
+      const element = this.doc.createElement("div");
+      element.classList.add("person");
+      element.innerHTML = `<strong>${item.name}</strong> ${this.formatMoney(
+        item.money
+      )}`;
+
+      this.main.appendChild(element);
+    });
+  }
+
+  formatMoney(number) {
+    return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+  }  
 }
 
-function doubleMoney() {
-  data = data.map((user) => {
-    return { ...user, money: user.money * 2 };
-  });
-
-  updateDOM();
-}
-
-function sortByRichest() {
-  data.sort((a, b) => b.money - a.money);
-  updateDOM();
-}
-
-function showMillionaires() {
-  data = data.filter((user) => user.money > 1000000);
-
-  updateDOM();
-}
-
-function calculateWealth() {
-  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
-
-  const wealthE1 = document.createElement("div");
-  wealthE1.innerHTML = `<h3> Total Wealth: <strong>${formatMoney(
-    wealth
-  )}</strong></h3>`;
-  main.appendChild(wealthE1);
-}
-
-function addData(obj) {
-  data.push(obj);
-
-  updateDOM();
-}
-
-function updateDOM(provideData = data) {
-  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
-
-  provideData.forEach((item) => {
-    const element = document.createElement("div");
-    element.classList.add("person");
-    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
-      item.money
-    )}`;
-
-    main.appendChild(element);
-  });
-}
-
-function formatMoney(number) {
-  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
-}
-
-addUserBtn.addEventListener("click", getRandomUser);
-doubleBtn.addEventListener("click", doubleMoney);
-sortBtn.addEventListener("click", sortByRichest);
-showMillionairesBtn.addEventListener("click", showMillionaires);
-calculateWealthBtn.addEventListener("click", calculateWealth);
+// Init the component
+new ArrayMethods(document);
